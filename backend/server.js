37f3949d3fe199d352d9f173d813cf69da9b8061
Sule Malik <abdulmaliksuleman75@gmail.com>
2026-01-12 14:43:03 +0000

    fix: explicit IP fingerprinting for Render

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ import sharingGroupRoutes from "./routes/sharingGroup.route.js";
 const app = express();
 
 if (NODE_ENV === "production") {
-  app.set("trust proxy", 3);
+  app.set("trust proxy", true); // This tells Express to trust the X-Forwarded-For header fully
 }
 
 const allowedOrigins = [
